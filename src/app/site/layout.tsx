2238942ee033935322b5385cb8125b57e9c78361
Harsh kumar <mister.harshkumar@gmail.com>
2024-04-15 22:04:31 +0530

    init Nextjs | shadcn | clerk , site , landing page

---
+++ src/app/site/layout.tsx
@@ -0,0 +1,18 @@
+
+import Navigation from '@/components/ui/sites/navigation'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import React from 'react'
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    // <ClerkProvider appearance={{ baseTheme: dark }}>
+      <main className="h-full  dark:bg-gradient-to-br dark:from-black dark:to-gray-700">
+        <Navigation />
+        {children}
+      </main>
+    // </ClerkProvider>
+  )
+}
+
+export default layout
